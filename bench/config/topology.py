065f6d04645782b36769fe2fdbb894c5f92f8e86
jay1999ke <jay1999ke@gmail.com>
2023-11-07 20:04:10 -0500

    Update Standard logic

--- bench/config/topology.py
+++ bench/config/topology.py
@@ -1,5 +1,6 @@
 from typing import List
 
+
 class ConfigKeys:
     latency = "latency"
     fail_rate = "failrate"
@@ -8,42 +9,48 @@ class ConfigKeys:
     type_standard = "standard"
     type_custom = "custom"
 
+
 class Config:
-    
+
     def __init__(self) -> None:
         self.type: str = ConfigKeys.type_standard
         self.latency_ms: int = 1
         self.fail_rate: float = 0.0
         self.stage_count: int = 1
 
+
 class Node:
 
     def __init__(self) -> None:
-        self.children : List[Node] = []
+        self.children: List[Node] = []
         self.latency_ms: int = 1
         self._fail_rate: float = 0.0
+        self.stage_index = 1
 
     @property
     def fail_rate(self) -> float:
         return self._fail_rate
-    
+
     @fail_rate.setter
     def fail_rate(self, value: float) -> None:
         self._fail_rate = value
         assert 0 <= value and value <= 100
 
     def createStandardChild(self, cfg: Config, stage_index: int) -> None:
-        if stage_index < cfg.stage_count:
+        if stage_index <= cfg.stage_count:
             node: Node = Node()
             node.fail_rate = cfg.fail_rate
             node.latency_ms = cfg.latency_ms
             self.children.append(node)
+            node.stage_index = stage_index
             node.createStandardChild(cfg, stage_index + 1)
+
 
 class Topology:
 
     def __init__(self) -> None:
         pass
+
 
 def makeCleanMap(map: dict) -> dict:
     result = {}
@@ -51,34 +58,35 @@ def makeCleanMap(map: dict) -> dict:
         result[key.lower()] = map[key]
     return result
 
+
 def parseConfig(config: dict) -> Config:
     cfg: Config = Config()
-    cfg.fail_rate = config.get(ConfigKeys.fail_rate)
-    cfg.latency_ms = config.get(ConfigKeys.latency)
-    cfg.stage_count = config.get(ConfigKeys.stage_count)
-    cfg.type = ConfigKeys.type_standard
-    return cfg
+    config: dict = makeCleanMap(config)
+
+    if config.get(ConfigKeys.type).lower() == ConfigKeys.type_standard.lower():
+        assert config.get(ConfigKeys.fail_rate) != None
+        assert config.get(ConfigKeys.latency) != None
+        assert config.get(ConfigKeys.stage_count) != None
+
+        cfg.fail_rate = config.get(ConfigKeys.fail_rate)
+        cfg.latency_ms = config.get(ConfigKeys.latency)
+        cfg.stage_count = config.get(ConfigKeys.stage_count)
+        cfg.type = ConfigKeys.type_standard
+        return cfg
+
 
 class StandardTopology(Topology):
 
     def __init__(self, config: Config) -> None:
         super().__init__()
-        config : dict = makeCleanMap(config)
-
-        assert config.get(ConfigKeys.fail_rate) != None
-        assert config.get(ConfigKeys.latency) != None
-        assert config.get(ConfigKeys.stage_count) != None
-        assert config.get(ConfigKeys.type).lower() == ConfigKeys.type_standard.lower()
-
         self.config: Config = config
         self.root: Node = self.createDAG()
+        assert self.config.stage_count >= 1
 
     def createDAG(self) -> Node:
         root: Node = Node()
         root.fail_rate = self.config.fail_rate
         root.latency_ms = self.config.latency_ms
-        root.createStandardChild(self.config, 1)
+        root.stage_index = 1
+        root.createStandardChild(self.config, 2)
         return root
-
-
-    
